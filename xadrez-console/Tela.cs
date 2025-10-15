bf742397358b3c415299ad09bea12bf36686c9df
Guilherme Giannini <guilhermegianninimelo@gmail.com>
2025-10-15 08:01:02 -0300

    Adicionando impressão de cor

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -1,4 +1,5 @@
 ﻿using tabuleiro;
+using xadrez_console.tabuleiro;
 
 namespace xadrez_console
 {
@@ -17,11 +18,28 @@ namespace xadrez_console
                     }
                     else 
                     {
-                        Console.Write(tab.peca(i, j) + " ");
+                        Tela.imprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine("  a b c d e f g h");
         }
+        public static void imprimirPeca (Peca peca)
+        {
+            if (peca.cor == Cor.Branca) 
+            {
+                Console.Write(peca);
+            }
+            else 
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
+        }
+            
     }
 }
